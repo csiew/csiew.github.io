1a3a05be82f3528c8b110ce629ecf0a6e2278d56
Clarence <cksie1@student.monash.edu>
2017-01-15 22:13:34 +0800

    Switched voice from US to Australian accent

--- hearye/main.js
+++ hearye/main.js
@@ -15,7 +15,7 @@ function grabHeadlines() {
             document.getElementById("content").innerHTML += "<hr /><p><a href='" + article.url + "' target='_blank'><img src='" + article.urlToImage + "' width=320px /><br /><br />" + article.title + "</a></p>";
         }
         teleprompt += "And that is all for the news. Check back later for more! Bye."
-        responsiveVoice.speak(teleprompt, "US English Female", {pitch: 1}, {rate: 1});
+        responsiveVoice.speak(teleprompt, "Australian Female", {pitch: 1}, {rate: 1});
     });
 }
 
