94f8e1f01b3d566c819946db7a520a0e32088e9a
csiew <clarence.siew@gmail.com>
2019-10-29 22:01:59 +1100

    Replace static header and footer with JS substitution

---
+++ main.js
@@ -0,0 +1,36 @@
+function initialise(pageName) {
+	this.setHeader(pageName);
+	this.setFooter();
+}
+
+function setHeader(pageName) {
+	let title = `
+		<h1>Clarence Siew</h1>
+		<div class="header-subtitle">Software Engineering student<br />Melbourne, Australia</div>
+	`;
+	let navBar = `
+		<div class="nav">
+			<ul>
+				<a href="index.html"` + this.isCorresponding("index", pageName) + `><li>Home</li></a>
+				<a href="projects.html"` + this.isCorresponding("projects", pageName) + `><li>Projects</li></a>
+				<a href="playlists.html"` + this.isCorresponding("playlists", pageName) + `><li>Playlists</li></a>
+				<a href="places.html"` + this.isCorresponding("places", pageName) + `><li>Places</li></a>
+				<a href="contact.html"` + this.isCorresponding("contact", pageName) + `><li>Contact</li></a>
+			</ul>
+		</div>
+	`;
+	
+	document.querySelector('header').innerHTML = title + navBar;
+}
+
+function isCorresponding(navName, pageName) {
+	if (navName === pageName) {
+		return "class='nav-active'";
+	}
+}
+
+function setFooter() {
+	let copyright = "<p>&copy; 2019 Clarence Siew</p>";
+	
+	document.querySelector('footer').innerHTML = copyright;
+}
