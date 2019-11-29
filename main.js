2322cc28f73d83d9cf333ebb0bc54066f0fd691c
csiew <clarence.siew@gmail.com>
2019-11-30 01:02:22 +1100

    Style overhaul

--- main.js
+++ main.js
@@ -5,25 +5,15 @@ function initialise(pageName) {
 
 function setHeader(pageName) {
 	let title = `
-		<div class="header-title">
-			<h1>Clarence Siew</h1>
-			<div class="header-subtitle">Software Engineering student<br />Melbourne, Australia</div>
+		<div id="masthead">
+			<a href="index.html">
+				<h1>Clarence Siew</h1>
+				<sub>Software Engineering student &bull; Melbourne</sub>
+			</a>
 		</div>
-	`;
-	let navBar = `
-		<div id="navlist" class="nav">
-			<ul>
-				<a href="index.html"` + this.isCorresponding("index", pageName) + `><li>Home</li></a>
-				<a href="projects.html"` + this.isCorresponding("projects", pageName) + `><li>Projects</li></a>
-				<a href="playlists.html"` + this.isCorresponding("playlists", pageName) + `><li>Playlists</li></a>
-				<a href="places.html"` + this.isCorresponding("places", pageName) + `><li>Places</li></a>
-				<a href="contact.html"` + this.isCorresponding("contact", pageName) + `><li>Contact</li></a>
-			</ul>
-		</div>
-		<hr />
 	`;
 	
-	document.querySelector('header').innerHTML = `<div class="header-container">` + title + navBar + `</div>`;
+	document.querySelector('header').innerHTML = title;
 }
 
 function isCorresponding(navName, pageName) {
@@ -33,7 +23,7 @@ function isCorresponding(navName, pageName) {
 }
 
 function setFooter() {
-	let copyright = "<p>&copy; Clarence Siew</p>";
+	let copyright = "<p>Copyright, 2019 Clarence Siew</p>";
 	
 	document.querySelector('footer').innerHTML = copyright;
 }
