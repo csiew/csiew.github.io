1c3464e1bb19a9c8d9fbd516428be5b62e141b7a
csiew <clarence.siew@gmail.com>
2020-06-23 05:17:27 +1000

    Skeumorphism galore

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ function setHeader() {
 	document.querySelector('header').innerHTML = `
 		<h1><a href='index.html'>Clarence Siew</a></h1>
 		<ul>
-			<a href='projects.html'><li>Projects</li></a>
-			<a href='places.html'><li>Places</li></a>
-			<a href='playlists.html'><li>Playlists</li></a>
+			<a class='button' href='projects.html'><li>Projects</li></a>
+			<a class='button' href='places.html'><li>Places</li></a>
+			<a class='button' href='playlists.html'><li>Playlists</li></a>
 		</ul>
 		`
 		+ currentContent;
@@ -22,8 +22,6 @@ function overlayWindowOpen(divId) {
 	document.body.style.overflow = 'hidden';
 	document.getElementById('overlayWindowContainer').style.visibility = 'visible';
 	document.getElementById('overlayWindowContainer').style.display = 'flex';
-	document.getElementById('overlayWindowContentContainer').style.visibility = 'visible';
-	document.getElementById('overlayWindowContentContainer').style.display = 'flex';
 	document.getElementById('overlayWindow-' + divId).style.visibility = 'visible';
 	document.getElementById('overlayWindow-' + divId).style.display = 'flex';
 	overlayWindowOpenSectionId = divId;
@@ -33,8 +31,6 @@ function overlayWindowClose() {
 	document.body.style.overflow = 'auto';
 	document.getElementById('overlayWindowContainer').style.visibility = 'hidden';
 	document.getElementById('overlayWindowContainer').style.display = 'none';
-	document.getElementById('overlayWindowContentContainer').style.visibility = 'hidden';
-	document.getElementById('overlayWindowContentContainer').style.display = 'none';
 	if (overlayWindowOpenSectionId) {
 		document.getElementById('overlayWindow-' + overlayWindowOpenSectionId).style.visibility = 'hidden';
 		document.getElementById('overlayWindow-' + overlayWindowOpenSectionId).style.display = 'none';
